932f891b2c9fe59b3133462e72a2365904943370
Ben Boral <ben.boral@bitrise.io>
2021-06-16 14:54:20 -0500

    script to add tests to project

---
+++ add_tests.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+ui_test_file=$(find . | grep BitriseTestUITests.swift)
+file_contents=$(cat $ui_test_file)
+editing_file=$(echo ${file_contents%\}*})
+for i in {1..100}
+do
+editing_file+="""
+    func testExample$i() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+""" >> editing_file
+done
+
+editing_file+="""
+}
+"""
+
+echo $editing_file > $ui_test_file
