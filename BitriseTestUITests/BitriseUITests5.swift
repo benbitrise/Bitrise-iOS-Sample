9e41f85d2f9a5ffdfbe74db7fa4015b03faa814f
Ben Boral <ben.boral@bitrise.io>
2021-07-08 19:47:13 -0500

    adds test classes for breaking up in parallelization

---
+++ BitriseTestUITests/BitriseUITests5.swift
@@ -0,0 +1,543 @@
+//
+//  BitriseTestUITests.swift
+//  BitriseTestUITests
+//
+//  Created by Damien Murphy on 1/28/21.
+//
+
+import XCTest
+
+class BitriseTestUITests5: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+    func testExample1() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample2() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample3() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample4() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample5() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample6() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample7() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample8() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample9() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample10() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample11() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample12() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample13() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample14() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample15() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample16() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample17() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample18() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample19() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample20() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample21() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample22() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample23() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample24() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample25() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample26() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample27() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample28() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample29() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample30() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample31() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample32() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample33() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample34() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample35() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample36() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample37() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample38() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample39() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample40() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample41() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample42() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample43() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample44() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample45() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample46() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample47() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample48() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample49() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample50() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample51() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample52() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample53() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample54() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample55() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample56() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample57() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample58() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample59() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample60() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample61() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample62() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample63() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample64() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample65() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample66() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample67() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample68() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample69() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample70() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample71() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample72() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample73() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample74() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample75() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample76() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample77() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample78() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample79() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample80() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample81() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample82() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample83() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample84() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample85() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample86() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample87() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample88() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample89() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample90() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample91() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample92() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample93() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample94() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample95() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample96() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample97() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample98() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample99() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testExample100() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+}
+
