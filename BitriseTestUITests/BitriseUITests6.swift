10c550092cdb5137e1f64bc9065c0558989af87c
Ben Boral <ben.boral@bitrise.io>
2021-07-08 20:32:58 -0500

    reduce number of tests and add another class

--- BitriseTestUITests/BitriseUITests6.swift
+++ BitriseTestUITests/BitriseUITests6.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class BitriseTestUITests3: XCTestCase {
+class BitriseTestUITests6: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -199,6 +199,5 @@ class BitriseTestUITests3: XCTestCase {
         app.launch()
     }
 
-  
 }
 
