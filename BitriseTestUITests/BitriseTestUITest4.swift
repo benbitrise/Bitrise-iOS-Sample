363bfd3084aaf9f6f1fdc531a81883a1b939f1c2
Ben Boral <ben.boral@bitrise.io>
2022-02-02 18:20:09 -0600

    adds more ui tests

--- BitriseTestUITests/BitriseTestUITest4.swift
+++ BitriseTestUITests/BitriseTestUITest4.swift
@@ -1,19 +1,22 @@
 //
-//  BitriseTestUITests.swift
+//  BitriseTestUITest4.swift
 //  BitriseTestUITests
 //
-//  Created by Damien Murphy on 1/28/21.
+//  Created by Ben Boral on 2/2/22.
 //
 
 import XCTest
 
-class BitriseTestUITests: XCTestCase {
+class BitriseTestUITest4: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -23,14 +26,11 @@ class BitriseTestUITests: XCTestCase {
     }
 
     func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
+    
     func testLaaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
--- BitriseTestUITests/BitriseTestUITests2.swift
+++ BitriseTestUITests/BitriseTestUITests2.swift
@@ -32,4 +32,13 @@ class BitriseTestUITests2: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
+    func testLaaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+    
 }
